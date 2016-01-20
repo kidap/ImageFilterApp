ff04d84009a5c34e97e2d0eee0627d9bea66d62b
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-20 00:12:19 -0500

    Added logic to upload images from camera or photo album

--- ImageFilterApp/ViewController.swift
+++ ImageFilterApp/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,66 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: New Photo button pressed
+    @IBAction func onNewPhoto(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "Uploading  Photo", message: "Select a source", preferredStyle: .ActionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { action in
+            self.showCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Album", style: .Default, handler: { action in
+            self.showPhotoLibrary()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { action in
+        }))
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
+    func showCamera(){
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    func showPhotoLibrary(){
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK: Image Picker delegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage]{
+            imageView.image = image as? UIImage
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //MARK: onFilter button pressed
+    @IBAction func onFilter(sender: AnyObject) {
+        
+    }
+    
+    //MARK: Compare button pressed
+    @IBAction func onCompare(sender: AnyObject) {
+        
+    }
+    
+    //MARK: Share button pressed
+    @IBAction func onShare(sender: AnyObject) {
+        
+    }
+    
 
 }
 
